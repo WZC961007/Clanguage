fa0653ae0845f9da6e04874d58b6ffcf2a540e35
rtWang <31734212+WZC961007@users.noreply.github.com>
2017-09-25 12:40:33 +0800

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,17 @@
+#include <iostream.h>
+ void main()
+{
+int i ;
+cout<<"please input i";
+cin>>i ;
+if (i>=1000)
+cout <<"A"<<endl;
+else 
+  if (i>=100)
+cout <<"B"<<endl;
+  else 
+    if (i>=10)
+cout <<"C"<<endl;
+    else 
+cout <<"D"<<endl;
+}
